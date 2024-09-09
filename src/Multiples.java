992564ed567b1ca9253a85053a27598077abcd71
RoyceLuo1216 <86991110+RoyceLuo1216@users.noreply.github.com>
2024-09-09 16:20:35 -0400

    finished

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        int i = 0;
+        int count = 0;
+        while ( i < 1000) {
+
+            count = count + counter(i);
+            i++;
+        }
+        System.out.println(count);
+    }
+
+    private static int counter(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            return 1;
+
+        } else if (divisibleBy3) {
+
+            return 1;
+
+        } else if (divisibleBy5) {
+
+            return 1;
+
+        } else {
+
+            return 0;
+
+        }
+
+
+    }
 }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduceSteps(100));
+    }
 
-public class Reducer {
+    public static int reduceSteps(int i){
+        int steps = 0;
+        int val = i;
+        while (val != 0){
+            if (val % 2 == 0){
+                val = val / 2;
+            }
+            else{
+                val = val - 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+
 }
